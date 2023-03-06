8243c3cd3755124cee32e9c406ae6a466dbed3f1
Aisyah shania putri <101912222+aisyahsh@users.noreply.github.com>
2023-03-06 15:01:04 +0700

    ujikom pt. 1

---
+++ modul/modul-auth/index.php
@@ -0,0 +1,118 @@
+<?php
+include('../../config/database.php');
+if (isset($_POST['cek'])) {
+    $pilihan = $_POST['pilihan']; //masyarakat atau petugas
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);
+    if ($pilihan == 'masyarakat') {
+        $q = mysqli_query($con, "SELECT * FROM `masyarakat` WHERE username = '$username' AND password = '$password' AND verifikasi = 1");
+        $r = mysqli_num_rows($q);
+        if ($r == 1) {
+            $d = mysqli_fetch_object($q);
+            @session_start();
+            $_SESSION['nik'] = $d->nik;
+            $_SESSION['username'] = $d->username;
+            $_SESSION['nama'] = $d->nama;
+            $_SESSION['telp'] = $d->telp;
+            $_SESSION['level'] = 'masyarakat';
+            @header('location:../../modul/modul-profile/');
+        } else {
+            echo '<div class="alert alert-danger alert-dismissable"><a href="" class="close" data-dismiss="alert">x</a> <strong class="text-white">Data salah atau belum di verifikasi</strong></div>';
+        }
+    } else if ($pilihan == 'petugas') {
+        $q = mysqli_query($con, "SELECT * FROM `petugas` WHERE username = '$username' AND password = '$password'");
+        $r = mysqli_num_rows($q);
+        if ($r == 1) {
+            $d = mysqli_fetch_object($q);
+            @session_start();
+            $_SESSION['username'] = $d->username;
+            $_SESSION['level'] = $d->level;
+            $_SESSION['id_petugas'] = $d->id_petugas;
+            @header('location:../../modul/modul-petugas/');
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include('../../assets/header.php') ?>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <title>
+    Login SISPENMAS
+  </title>
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+  <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+  <link id="pagestyle" href="../assets/css/material-dashboard.css?v=3.0.4" rel="stylesheet" />
+</head>
+
+<body>
+<main class="main-content  mt-0">
+    <div class="page-header align-items-start min-vh-100" style="background-image: url('https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80');">
+      <span class="mask bg-gradient-dark opacity-6"></span>
+      <div class="container my-auto">
+        <div class="row">
+          <div class="col-lg-4 col-md-8 col-12 mx-auto">
+            <div class="card z-index-0 fadeIn3 fadeInBottom">
+              <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
+                  <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Login SISPENMAS</h4>
+                  <div class="row mt-3">
+                    <div class="col-2 text-center ms-auto">
+                      <a class="btn btn-link px-3" href="javascript:;">
+                        <i class="fa fa-facebook text-white text-lg"></i>
+                      </a>
+                    </div>
+                    <div class="col-2 text-center px-1">
+                      <a class="btn btn-link px-3" href="javascript:;">
+                        <i class="fa fa-github text-white text-lg"></i>
+                      </a>
+                    </div>
+                    <div class="col-2 text-center me-auto">
+                      <a class="btn btn-link px-3" href="javascript:;">
+                        <i class="fa fa-google text-white text-lg"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="card-body">
+                <form role="form" class="text-start">
+                     <div class="input-group input-group-outline my-3">
+                    <label class="form-label">Username</label>
+                    <input type="email" class="form-control">
+                  </div>
+                  <div class="input-group input-group-outline mb-3">
+                    <label class="form-label">Password</label>
+                    <input type="password" class="form-control">
+                  </div>
+                  <label for="pilihan">.:LOGIN SEBAGAI:.</label>
+                  <div class="input-group input-group-outline mb-3">
+                  <select class="form-control" name="pilihan">
+                  <option value="masyarakat">Masyarakat</option>
+                  <option value="petugas">Petugas</option>
+                  </select>
+                  <div class="text-center">
+                    <button type="button" class="btn bg-gradient-primary w-100 my-4 mb-2">Masuk</button>
+                  </div>
+                  <p class="mt-4 text-sm text-center">
+                    Belum verifikasi akun?
+                    <a href="../modul/register.php" class="text-primary text-gradient font-weight-bold">Daftar disini</a>
+                  </p>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <?php include('../../assets/footer.php') ?>
+  </body>
---
+++ modul/modul-auth/register.php
@@ -0,0 +1,101 @@
+<?php
+include('../../config/database.php');
+if (isset($_POST['simpan'])) {
+    $nik = $_POST['nik'];
+    $nama = $_POST['nama'];
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);
+    $telp = $_POST['telp'];
+    $q = mysqli_query($con, "INSERT INTO `masyarakat` (nik, nama, username, password, telp, verifikasi) VALUES ('$nik', '$nama', '$username', '$password', $telp, 0)");
+    if ($q) {
+        echo  '<div class="alert alert-success alert-dismissable">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
+                Registrasi Berhasil Tunggu verifikasi oleh admin
+                </div>';
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include('../../assets/header.php') ?>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <title>
+    Register SISPENMAS
+  </title>
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+  <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+  <link id="pagestyle" href="../assets/css/material-dashboard.css?v=3.0.4" rel="stylesheet" />
+</head>
+
+<body class="">
+<main class="main-content  mt-0">
+    <section>
+      <div class="page-header min-vh-100">
+        <div class="container">
+          <div class="row">
+            <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 start-0 text-center justify-content-center flex-column">
+              <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center" style="background-image: url('../assets/img/illustrations/illustration-signup.jpg'); background-size: cover;">
+              </div>
+            </div>
+            <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column ms-auto me-auto ms-lg-auto me-lg-5">
+              <div class="card card-plain">
+                <div class="card-header">
+                  <h4 class="font-weight-bolder">Registrasi masyarakat</h4>
+                  <p class="mb-0">Masukkan data untuk registrasi/p>
+                </div>
+                <div class="card-body">
+                  <form role="form">
+                    <div class="input-group input-group-outline mb-3">
+                      <label class="form-label">Nik</label>
+                      <input type="text" class="form-control">
+                    </div>
+                    <div class="input-group input-group-outline mb-3">
+                      <label class="form-label">Nama lengkap sesuai KTP</label>
+                      <input type="text" class="form-control">
+                    </div>
+                    <div class="input-group input-group-outline mb-3">
+                      <label class="form-label">Username</label>
+                      <input type="text" class="form-control">
+                    </div>
+                    <div class="input-group input-group-outline mb-3">
+                      <label class="form-label">Password</label>
+                      <input type="password" class="form-control">
+                    </div>
+                    <div class="input-group input-group-outline mb-3">
+                      <label class="form-label">Telp</label>
+                      <input type="number" class="form-control">
+                    </div>
+                      </label>
+                    </div>
+                    <div class="text-center">
+                      <button type="button" class="btn btn-lg bg-gradient-primary btn-lg w-100 mt-4 mb-0">Sign Up</button>
+                    </div>
+                  </form>
+                </div>
+                <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                  <p class="mb-2 text-sm mx-auto">
+                    Sudah verifikasi akun?
+                    <a href="../modul/index.php" class="text-primary text-gradient font-weight-bold">Masuk disini</a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+
+  <?php include('../../assets/footer.php') ?>
+
+</body>
+</html>
